ac5f789b87c5809734cde037bfdc39e536012241
ddhmed <799390224@qq.com>
2019-06-26 22:02:19 +0800

    Update causalin_ference.py

--- Code/causalin_ference.py
+++ Code/causalin_ference.py
@@ -15,129 +15,76 @@ from sklearn.metrics import roc_auc_score
 from scipy.stats import norm, bernoulli, pearsonr
 import dowhy
 from dowhy.do_why import CausalModel
-import matplotlib.pyplot as plt
 DefaultStdout = sys.stdout
 
-def getPvalue(v0, data, treatment, outcome, common_causes, t=300):
+# Estimate causal effect
+def causal_effect_estimate(data, treatment, outcome, common_causes):
+    try:
+        model= CausalModel(data=data,treatment=treatment,outcome=outcome,common_causes=common_causes)
+        identified_estimand = model.identify_effect()
+        estimate = model.estimate_effect(identified_estimand, method_name="backdoor.linear_regression")
+        value = estimate.value
+        return value
+    except:
+        return None
+
+# p-value of causal effect by permutation test
+def get_p_value(e0, data, treatment, outcome, common_causes, times=300):
     p = 1
-    rdata = copy.deepcopy(data)
+    random_data = copy.deepcopy(data)
     trycount = 0
-    for i in range(t-1):
-        try:
-            rdata.loc[:, treatment] = random.sample(list(rdata.loc[:, treatment]), len(rdata.index))
-            rdata.loc[:, outcome] = random.sample(list(rdata.loc[:, outcome]), len(rdata.index))
-            model= CausalModel(data=rdata,treatment=treatment,outcome=outcome,common_causes=common_causes)
-            identified_estimand = model.identify_effect()
-            estimate = model.estimate_effect(identified_estimand, method_name="backdoor.linear_regression")
-            value = estimate.value
-            if value >= v0:
-                p+=1
-        except:
-            p+=1
-            trycount += 1
-    p = p/float(t)
-    if trycount>0.5*t:
-        return 1.0
+    for i in range(times-1):
+        random_data.loc[:, treatment] = random.sample(list(random_data.loc[:, treatment]), len(random_data.index))
+        random_data.loc[:, outcome] = random.sample(list(random_data.loc[:, outcome]), len(random_data.index))
+        random_effect = causal_effect_estimate(random_data, treatment, outcome, common_causes)
+        if random_effect == None or random_effect > e0:
+            p += 1
+    p = p/float(times)
     return p if p<0.5 else 1-p
-    
 
-def getCausalRelated_from_pattern2(step, aucm, data, pattern, rt=100, islog=False):
-    if not islog:
-        file = open('logger.log', 'w')
-        sys.stdout = file
-    e_matrix = np.zeros(shape=(len(data.columns), len(data.columns)))
-    p_matrix = np.zeros(shape=(len(data.columns), len(data.columns)))
-    DG=nx.DiGraph()
+# create prior network
+def prior_network(pattern):
+    network=nx.DiGraph()
     links = []
     for i in range(len(pattern)):
         for j in range(len(pattern[0])):
             if i!=j and pattern[i, j]==1:
                 links.append((j, i))
-    DG.add_edges_from(links)
+    network.add_edges_from(links)
+    return network
+
+# get confounder of treatment and target / ancestors
+def confounder(network, i, j):
+    try:
+        causes_node1 = nx.ancestors(network, i)
+        causes_node2 = nx.ancestors(network, j)
+        common_causes = list(causes_node1.intersection(causes_node2))
+        common_causes = [data.columns[c] for c in common_causes]
+    except:
+        common_causes = []
+    return common_causes
+
+# causal inference
+def causal_inference_from_prior(data, pattern):
+    e_matrix = np.zeros(shape=(len(data.columns), len(data.columns))) # effect matrix
+    p_matrix = np.zeros(shape=(len(data.columns), len(data.columns))) # p-value matrix
+    # create prior microbial network
+    network = prior_network(pattern)
+    # estimate causal effect and p-value for microbial pairs one by one
     for i in range(len(data.columns)):
         for j in range(len(data.columns)):
             if i == j:
                 continue
             treatment = data.columns[i]
             outcome = data.columns[j]
-            
-            try:
-                causes_node1 = nx.ancestors(DG, i)
-                causes_node2 = nx.ancestors(DG, j)
-                common_causes = list(causes_node1.intersection(causes_node2))
-                common_causes = [data.columns[c] for c in common_causes]
-            except:
-                common_causes = []
-            #common_causes = [data.columns[c] for c in range(len(data.columns)) if pattern[i][c]==1 and pattern[j][c]==1]
-            try:
-                model= CausalModel(data=data,treatment=treatment,outcome=outcome,common_causes=common_causes)
-                identified_estimand = model.identify_effect()
-                estimate = model.estimate_effect(identified_estimand, method_name="backdoor.linear_regression")
-                e_matrix[j][i] = estimate.value
-                if e_matrix[j][i]==0:
-                    p_matrix[j][i] = 1
-                else:
-                    p_matrix[j][i] = getPvalue(estimate.value, data, treatment, outcome, common_causes, t=100)
-            except:
-                e_matrix[j][i] = 0
-                p_matrix[j][i] = 1
-            if not islog:
-                sys.stdout = DefaultStdout
-                print(step, aucm, 'causal', i, j, e_matrix[j][i], p_matrix[j][i])
-                #print(common_causes)
-                sys.stdout = file
-    if not islog:
-        sys.stdout = DefaultStdout
-        file.close()
+            common_causes = confounder(network, i, j) # cofounders
+            effect = causal_effect_estimate(data, treatment, outcome, common_causes)
+            e_matrix[j][i] = effect if effect != None else 0
+            p_matrix[j][i] = get_p_value(e0, data, treatment, outcome, common_causes, times=times) if effect != None else 1
     return e_matrix, p_matrix
 
-def getCausalRelated_from_pattern(step, aucm, data, pattern, rt=100, islog=False):
-    e_matrix = np.zeros(shape=(len(data.columns), len(data.columns)))
-    p_matrix = np.zeros(shape=(len(data.columns), len(data.columns)))
-    DG=nx.DiGraph()
-    links = []
-    for i in range(len(pattern)):
-        for j in range(len(pattern[0])):
-            if i!=j and pattern[i, j]==1:
-                links.append((j, i))
-    DG.add_edges_from(links)
-    for i in range(len(data.columns)):
-        for j in range(len(data.columns)):
-            if i == j:
-                continue
-            treatment = data.columns[i]
-            outcome = data.columns[j]
-            
-            try:
-                causes_node1 = nx.ancestors(DG, i)
-                causes_node2 = nx.ancestors(DG, j)
-                common_causes = list(causes_node1.intersection(causes_node2))
-                common_causes = [data.columns[c] for c in common_causes]
-            except:
-                common_causes = []
-            #common_causes = [data.columns[c] for c in range(len(data.columns)) if pattern[i][c]==1 and pattern[j][c]==1]
-            try:
-                model= CausalModel(data=data,treatment=treatment,outcome=outcome,common_causes=common_causes)
-                identified_estimand = model.identify_effect()
-                estimate = model.estimate_effect(identified_estimand, method_name="backdoor.linear_regression")
-                e_matrix[j][i] = estimate.value
-                if e_matrix[j][i]==0:
-                    p_matrix[j][i] = 1
-                else:
-                    p_matrix[j][i] = getPvalue(estimate.value, data, treatment, outcome, common_causes, t=100)
-            except:
-                e_matrix[j][i] = 0
-                p_matrix[j][i] = 1
-    return e_matrix, p_matrix
-
-def data_check(data):
-    sel_columns = []
-    for c in data.median().index:
-        if data.median()[c]>0:
-            sel_columns.append(c)
-    return data.loc[:, sel_columns]
-
-def get_auc(e_matrix, parameter):
+# network structrue similarity
+def network_similarity(e_matrix, parameter):
     parameter = np.array(parameter)
     e_matrix = np.array(e_matrix)
     plist = []
@@ -155,62 +102,47 @@ def get_auc(e_matrix, parameter):
     auc = roc_auc_score(plist, elist)
     return auc
 
+# load abundance data
+def load_abundance(path):
+    return pd.read_table(path, index_col=0).T
 
-def getRelated(data, islog=False):
-    e_matrix = np.zeros(shape=(len(data.columns), len(data.columns)))
-    p_matrix = np.zeros(shape=(len(data.columns), len(data.columns)))
-    for i in range(len(data.columns)):
-        for j in range(len(data.columns)):
-            if i == j:
-                continue
-            valuei = data.iloc[:, i]
-            valuej = data.iloc[:, j]
-            r, p = pearsonr(valuei, valuej)
-            e_matrix[j][i] = r
-            p_matrix[j][i] = p
-            if not islog:
-                print('relation', i, j, e_matrix[j][i], p_matrix[j][i])
-    if not islog:
-        sys.stdout = DefaultStdout
-    return e_matrix, p_matrix
+# load prior network calculated by SparCC
+def load_proir_network(path, threshold=0.01):
+    sparcc = pd.read_table(path, index_col=0)
+    pattern = (np.array(sparcc)<threshold)*1.0
+    return pattern
+
+# main run of causal inference
+def run_causal_inference(data, prior, output_path='Result/', steps = 50, learn_rate=0.2,
+                         times=100, min_threshold=0.05, max_threshold=0.3, simi_threshold=0.85):
+    for step in range(steps):
+        inter_indicator = bernoulli.rvs(prior)
+        e_matrix, p_matrix = causal_inference_from_prior(data, inter_indicator)
+        # output
+        pd.DataFrame(e_matrix, columns=Species, index=Species).to_csv(output_path+dataname+'_ematrix_'+str(step)+'.csv')
+        pd.DataFrame(p_matrix, columns=Species, index=Species).to_csv(output_path+dataname+'_pmatrix_'+str(step)+'.csv')
+        pd.DataFrame(prior, columns=Species, index=Species).to_csv(output_path+dataname+'_prior_'+str(step)+'.csv')
+        simi_score = network_similarity(1-p_matrix, inter_indicator)
+        prior += learn_rate*(p_matrix<=min_threshold)*np.random.uniform(low=0, high=1, size=(prior.shape[0], prior.shape[1]))
+        prior -= learn_rate*(p_matrix>=max_threshold)*np.random.uniform(low=0, high=1, size=(prior.shape[0], prior.shape[1]))
+        prior = prior.clip(0, 1)
+        if simi_score >= simi_threshold:
+            break
+    return True
 
 if __name__ == '__main__':
-    dataname = 'NASH'
-    data = pd.read_table('Data/'+dataname+'.txt', index_col=0)
-    species2 = set(data.index)
-    print(len(species2))
-    Species = sorted(species2)
-    
-    #####
-    dataname = 'NASH'
-    relate_data = pd.read_table('Data/'+dataname+'_ra.txt', index_col=0).T
-    relate_data = relate_data.loc[:, Species]
-
-    sparcc = pd.read_table('Data/'+dataname+'_p.txt', index_col=0)
-    sparcc = sparcc.loc[Species, Species]
-    print(np.array(sparcc))
-
-    pattern = (np.array(sparcc)<0.01)*1.0
-    print(pattern)
-    aucm = 0.1
-    learn = 0.2
-    f = open('Result/'+dataname+'_log.txt', 'w')
-    rt=100
-    for step in range(55):
-        p_sel = bernoulli.rvs(pattern)
-        new_e_matrix, new_p_matrix = getCausalRelated_from_pattern(step, aucm, relate_data, p_sel, rt=rt, islog=False)
-        print(pattern)
-        pd.DataFrame(new_e_matrix, columns=Species, index=Species).to_csv('Result/'+dataname+'_common_ematrix_step'+str(step)+'.csv')
-        pd.DataFrame(new_p_matrix, columns=Species, index=Species).to_csv('Result/'+dataname+'_common_pmatrix_step'+str(step)+'.csv')
-        pd.DataFrame(pattern, columns=Species, index=Species).to_csv('Result/'+dataname+'_common_pattern_step'+str(step)+'.csv')
-        new_aucm = get_auc(1-new_p_matrix, p_sel)
-        #if random.random()<0.5*(new_aucm/aucm):
-        e_matrix = new_e_matrix
-        p_matrix = new_p_matrix
-        aucm = new_aucm
-        pattern += learn*(p_matrix<=0.05)*np.random.uniform(low=0, high=1, size=(pattern.shape[0], pattern.shape[1]))
-        pattern -= learn*(p_matrix>=0.3)*np.random.uniform(low=0, high=1, size=(pattern.shape[0], pattern.shape[1]))
-        pattern = pattern.clip(0, 1)
-        f.write(str(step)+'\t'+str(aucm)+'\n')
-        f.flush()
-    f.close()
+    path_abundace = 'Data/NASH.txt'
+    path_SparCC = 'Data/NASH_ra.txt'
+    output_path='Result/'
+    steps = 100
+    learn_rate = 0.2
+    times = 1001 # permutation test
+    min_threshold = 0.05
+    max_threshold = 0.3
+    simi_threshold = 0.85
+    # load abudance data and SparCC prior network
+    data = load_abundance(path_abundace)
+    prior = load_proir_network(path_SparCC, threshold=0.01)
+    # run causal inference
+    run_causal_inference(data, prior, output_path, steps, learn_rate,
+                         times, min_threshold, max_threshold, simi_threshold)
